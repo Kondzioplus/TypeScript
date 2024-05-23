5631c1a71e13285d6ff5d1b69eb6306eaa13debb
Kondzioplus <111319052+Kondzioplus@users.noreply.github.com>
2024-05-23 18:19:14 +0200

    Modyfikatory dostępu

--- dist/pizzeria.js
+++ dist/pizzeria.js
@@ -2,7 +2,17 @@
 //tworzymy klasę
 class Pizzeria {
     constructor(name) {
+        this.pizzasInOrder = [];
+        this.maxPizzasInOven = 10;
+        this.recipes = [];
         this.name = name;
+    }
+    order(pizza) {
+        this.pizzasInOrder.push(pizza);
+    }
+    isOvenFull() {
+        return this.pizzasInOrder.length > this.maxPizzasInOven;
     }
 }
 const laStrada = new Pizzeria("LaStrada");
+laStrada.order("Havanian Pizza");
--- src/pizzeria.ts
+++ src/pizzeria.ts
@@ -1,10 +1,21 @@
 //tworzymy klasę
 
 class Pizzeria {
-    name;
+    readonly name;
+    private pizzasInOrder = [];
+    private maxPizzasInOven = 10;
+    protected recipes = [];
+
     constructor(name: string) {
         this.name = name;
+    }
+    order(pizza) {
+        this.pizzasInOrder.push(pizza);
+    }
+    private isOvenFull() {
+        return this.pizzasInOrder.length > this.maxPizzasInOven;
     }
 }
 
 const laStrada = new Pizzeria("LaStrada");
+laStrada.order("Havanian Pizza");
