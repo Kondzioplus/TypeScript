effcfdff5637cbab7a633c3c02a88a2ca0b7e68d
Kondzioplus <111319052+Kondzioplus@users.noreply.github.com>
2024-05-24 19:53:09 +0200

    instalacja dodatkowych Types z githuba

---
+++ dist/dom.js
@@ -0,0 +1,5 @@
+"use strict";
+//instalacja dodatkowych typów z githuba.
+//npm install @type/"nazwa" --save-dev.
+const $container = $('.container');
+$container.addClass('abc');
---
+++ src/dom.ts
@@ -0,0 +1,5 @@
+//instalacja dodatkowych typów z githuba.
+//npm install @type/"nazwa" --save-dev.
+
+const $container = $('.container');
+$container.addClass('abc');
